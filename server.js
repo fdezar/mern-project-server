dfc34fa30ffbc6f2eab7b58ba72332b1770f3795
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-03-11 22:03:29 +0100

    vercel

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require(".");
+const app = require("./app");
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
